76c8dace3a2ec1b1329e81cb6fde43716f26ebbf
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-12-02 20:16:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Possible paths between 2 vertices/possible-paths-between-2-vertices.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count paths between two vertices in a directed graph.
+    int help(int src, int dst, vector<int> adj[]){
+    if(src==dst) return 1;
+    int ans=0;
+    for(auto child:adj[src])
+        ans+=help(child, dst, adj);
+    return ans;
+
+    
+}
+    int countPaths(int V, vector<int> adj[], int src, int dst) {
+    return help(src, dst, adj);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // adj[v].push_back(u);
+        }
+        int source, destination;
+        cin >> source >> destination;
+        Solution obj;
+        cout << obj.countPaths(V, adj, source, destination) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

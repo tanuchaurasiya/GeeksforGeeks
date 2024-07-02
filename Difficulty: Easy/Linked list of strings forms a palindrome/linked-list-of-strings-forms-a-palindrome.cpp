827f41fba91a14aed50863a0f103a4a4eff4af65
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-07-02 22:32:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Code by Shubham Joshi
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    string data;
+    Node *next;
+
+    Node(string x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *newNode(string str) {
+    Node *new_node = new Node(str);
+    new_node->next = NULL;
+    return new_node;
+}
+
+void print(Node *root) {
+    Node *temp = root;
+
+    while (temp != NULL) {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+struct Node
+{
+    string data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    bool compute(Node* head) {
+        // Your code goes here
+        
+       vector<char> nodesvalue;
+       
+       Node* temp=head;
+       while(temp!=NULL){
+           vector<char>  charArray(temp->data.begin(),temp->data.end());
+           for(char c : charArray)
+           nodesvalue.push_back(c);
+           temp=temp->next;
+       }
+       int n = nodesvalue.size();
+       
+       for(int i=0;i<n/2;i++)
+       {
+           if(nodesvalue[i]!=nodesvalue[n-1-i])
+           return false;
+       }
+       return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int K;
+        cin >> K;
+        struct Node *head = NULL;
+        struct Node *temp = head;
+
+        for (int i = 0; i < K; i++) {
+            string str = "";
+            cin >> str;
+            if (head == NULL)
+                head = temp = new Node(str);
+            else {
+                temp->next = new Node(str);
+                temp = temp->next;
+            }
+        }
+        Solution ob;
+        bool ans = ob.compute(head);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

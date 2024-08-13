4b354ca391017f26bf5a5ee2fd94d0a49b038b36
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-08-13 15:15:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int> arr, int n, long long s) {
+        unordered_map<int, int> mp;
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==s) return {1,i+1};
+            
+            if(mp[sum-s]!=0) return {mp[sum-s]+1,i+1};
+            if(mp[sum]==0) mp[sum]=i+1;
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        long long s;
+        cin >> n >> s;
+        vector<int> arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> res;
+        res = ob.subarraySum(arr, n, s);
+
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

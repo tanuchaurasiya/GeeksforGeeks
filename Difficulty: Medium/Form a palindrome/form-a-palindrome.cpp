4f66a534475f2a3954b9bcef71e18d8ad081a611
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-09-01 00:50:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -9,27 +9,26 @@ using namespace std;
 
 class Solution{   
 public:
-    int solve(string &s, int &n, int i, int j, vector<vector<int>>&dp){
-        if(i == j || i>j)
-            return 0;
-        
-        if(dp[i][j]!= -1){
-            return dp[i][j];
+    int findMinInsertions(string text1){
+        string text2="";
+        int n = text1.length();
+        for(int i=n-1; i>=0;i--){
+            text2+=text1[i];
+        }
+        // cout<<text2<<endl;
+       vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
         
-        if(s[i] == s[j]){
-            dp[i][j] =  solve(s,n,i+1,j-1, dp);
-        }else{
-            dp[i][j] =  1 + min(solve(s,n,i,j-1, dp), solve(s,n,i+1,j, dp));
-        }
-        
-        return dp[i][j];
-    }
-    int findMinInsertions(string s){
-        int n = s.length();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        // code here 
-         return solve(s, n, 0, s.length()-1, dp);
+        return n-dp[n][n];
     }
 };
 

bbce20ee6230fcb7ff05b7d430382a4eb4853eea
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-09-01 12:07:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -11,25 +11,20 @@ class Solution {
   string s1;
   string s2;
     int help(int i, int j, vector<vector<int>> &dp){
-        if(i==n1 && j==n2) return 0;
-        if(i==n1){
-            return (n2-j);
+        if(i==0){
+            return j;
         }
-        if(j==n2) {
-            return (n1-i);
+        if(j==0) {
+            return i;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        int x=INT_MAX;
-        int y=INT_MAX;
-        if(s1[i]==s2[j]){
-            x=help(i+1,j+1,dp);
+        if(s1[i-1]==s2[j-1]){
+            dp[i][j]=help(i-1,j-1,dp);
         }
         
         else{
-            y=1+min(help(i+1,j+1,dp), min(help(i+1,j,dp), help(i,j+1,dp)));
-            
+            dp[i][j]=1+min(help(i-1,j-1,dp), min(help(i-1,j,dp), help(i,j-1,dp)));
         }
-        dp[i][j] = min(x, y);
         return dp[i][j];
     }
     int editDistance(string str1, string str2) {
@@ -37,8 +32,8 @@ class Solution {
         s2=str2;
         n1=s1.length();
         n2=s2.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return help(0,0,dp);
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        return help(n1,n2,dp);
     }
 };
 

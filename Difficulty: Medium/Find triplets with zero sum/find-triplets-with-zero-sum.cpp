926e9348651b18cc4b64e974ff5dd15028b458ca
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-09-03 16:20:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
+++ Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
@@ -14,22 +14,29 @@ class Solution{
     //Function to find triplets with zero sum.
     bool findTriplets(int arr[], int n)
     { 
-        int i=0;
+        int i=n-1;
         sort(arr, arr+n);
-        while(i<n){
-            int j=i+1;
-            int k=n-1;
+        // for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+        // cout<<endl;
+        while(i>=0){
+            int j=i-1;
+            int k=0;
             
-            while(j<k){
-                if(arr[i]+arr[j]+arr[k]==0) return true;
-                else if(arr[i]+arr[j]+arr[k]>0){
-                    k--;
+            while(k<j){
+                // cout<<"i="<<i<<" j="<<j<<" k="<<k<<endl;
+                if(arr[i]+arr[j]+arr[k]==0){
+                    // cout<<"Res="<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
+                    return true;
                 }
-                else{
-                    j++;
+                else if(arr[i]+arr[j]+arr[k]<0){
+                    k++;
                 }
+                else {
+                    j--;
+                }
+                
             }
-            i++;
+            i--;
         }
         return 0;
     }

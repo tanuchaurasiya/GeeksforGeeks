14bd07f1fa2b2dd1b82b39c5bcf61ccb1158474f
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-04-04 21:14:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    
+    bool dfs(int node, int par, vector<vector<int>>& adj, vector<int> &visited){
+        visited[node]=1;
+        for(auto child: adj[node]){
+            if(!visited[child]){
+                if(dfs(child, node, adj, visited)==true)
+                    return true;
+            }
+            
+            else if(child!=par) return true;
+        }
+        return false;
+    }
+	bool isCycle(int V, vector<vector<int>>& adj) {
+	    vector<int> visited(V);
+	    for(int i=0;i<V;i++) {
+	        if(!visited[i])
+	        {
+	            if (dfs(i, -1, adj, visited)) return true;
+	        }
+	    }
+	    return false;
+	}
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
+        }
+
+        Solution obj;
+        bool ans = obj.isCycle(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

f2431c23356853dd1c0269a5a5401d90ae060d70
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-04-30 22:01:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
@@ -12,14 +12,6 @@ struct Node {
     }
 };
 
-void printList(Node *node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
 void loopHere(Node *head, Node *tail, int position) {
     if (position == 0)
         return;
@@ -32,6 +24,7 @@ void loopHere(Node *head, Node *tail, int position) {
 
 
 // } Driver Code Ends
+
 /*
 
 struct Node {
@@ -70,6 +63,8 @@ class Solution {
     }
 };
 
+
+
 //{ Driver Code Starts.
 
 int main() {
@@ -98,6 +93,7 @@ int main() {
 
         Solution ob;
         cout << ob.countNodesinLoop(head) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }

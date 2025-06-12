2f4cc73d14e205ff93beeae6f84ed327ba797928
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-06-12 09:58:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K closest elements/k-closest-elements.cpp
+++ Difficulty: Medium/K closest elements/k-closest-elements.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 struct Compare
@@ -25,7 +17,8 @@ struct Compare
 class Solution {
   public:
   
-    vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        int n=arr.size();
         priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
         for(int i=0;i<n;i++){
             if(arr[i]!=x) pq.push({abs(x-arr[i]), arr[i]});
@@ -42,28 +35,3 @@ class Solution {
     }
 };
 
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k, x;
-        cin >> n;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        cin >> k >> x;
-        Solution ob;
-        auto ans = ob.printKClosest(arr, n, k, x);
-        for (auto e : ans) {
-            cout << e << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends

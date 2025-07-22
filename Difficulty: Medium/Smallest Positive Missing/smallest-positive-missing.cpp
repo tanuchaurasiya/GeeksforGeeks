17a1788320891a67f55da00c1cece539b42119ac
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-07-22 22:23:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        unordered_map<int,int> mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+            mp[arr[i]] = 1;
+        
+        for(int i=1;i<=n+1;i++)
+            if(mp[i]==0) return i; 
+            
+        return -1;
+    }
+};

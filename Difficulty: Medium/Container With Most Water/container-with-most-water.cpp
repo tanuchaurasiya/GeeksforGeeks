f17d70ca520d817c6b1332163d38e292baf06b9b
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-08-31 23:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -1,49 +1,22 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-// int maxArea(int A[], int len);
-
-// } Driver Code Ends
-//User function template for C++
-
-long long maxArea(long long height[], int n)
-{
-    long long l=0;
-    long long r=n-1;
-
-    long long res=0;
-
-    while(l<r){
-        res=max(res, (r-l)*min(height[l], height[r]));
-        if(height[l]>height[r]){
-            r--;
+class Solution {
+  public:
+    int maxWater(vector<int> &height) {
+        int n=height.size();
+        int l=0;
+        int r=n-1;
+    
+        int res=0;
+    
+        while(l<r){
+            res=max(res, (r-l)*min(height[l], height[r]));
+            if(height[l]>height[r]){
+                r--;
+            }
+            else{
+                l++;
+            }
         }
-        else{
-            l++;
-        }
+        return res;
+        
     }
-    return res;
-}
-
-//{ Driver Code Starts.
-
-// Driver code
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-    {
-        int n;
-        cin>>n;
-        long long arr[n];
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        cout<<maxArea(arr,n)<<endl;
-    }
-return 0;
-}
-
-// } Driver Code Ends
+};

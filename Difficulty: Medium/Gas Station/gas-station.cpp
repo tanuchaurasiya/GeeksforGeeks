9320b01f95c3c668d842d5b75a4dad01537d0e27
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-09-14 13:33:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -0,0 +1,28 @@
+
+
+/*You are required to complete this method*/
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int n = gas.size();
+        int totalgas = 0;
+        int totalcost = 0;
+        for(int i=0;i<n;i++){
+            totalgas=totalgas+gas[i];
+            totalcost=totalcost+cost[i];
+        }
+        if(totalgas<totalcost) return -1;
+        int currgas = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            currgas = currgas+(gas[i]-cost[i]);
+            if(currgas<0){
+                currgas=0;
+                ans = i+1;
+            }
+        }
+        return ans;
+    }
+};
+
+

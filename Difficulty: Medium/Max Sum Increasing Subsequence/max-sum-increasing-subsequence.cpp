fa018086e1eea8f3f9733f0545dffcaf2494cfe7
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2025-11-17 20:49:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.cpp
+++ Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.cpp
@@ -1,22 +1,21 @@
 class Solution {
   public:
-    int f(int i, int j, vector<vector<int>>& dp, vector<int>& nums){
-        if(i==nums.size()){
-            return 0;
-        }
-        if(dp[i][j+1]!=-1){
-            return dp[i][j+1];
-        }
-        int l=f(i+1, j, dp, nums);
-        if(j==-1 || nums[i]>nums[j]){
-            l=max(l, nums[i]+f(i+1, i, dp, nums));
-        }
-        return dp[i][j+1]=l;
+    int n;
+    vector<vector<int>> dp;
+    int help(int prev,int curr, vector<int>& arr)
+    {
+        if(curr==n) return 0;
+        if(dp[prev+1][curr]!=-1) return dp[prev+1][curr];
+        int take=0;
+        int nottake=0;
+        
+        if(prev==-1 || arr[curr]>arr[prev]) take=arr[curr]+help(curr, curr+1, arr);
+        nottake= help(prev, curr+1, arr);
+        return dp[prev+1][curr]=max(take, nottake);
     }
-    int maxSumIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n, vector<int>(n+1, -1));
-        int r=f(0, -1, dp, nums);
-        return r;
+    int maxSumIS(vector<int>& arr) {
+        n=arr.size();
+        dp.resize(n, vector<int>(n,-1));
+        return help(-1, 0, arr);
     }
 };

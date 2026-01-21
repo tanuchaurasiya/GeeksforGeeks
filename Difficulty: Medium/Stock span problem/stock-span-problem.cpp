65ffc909a1815b963c498ed373bcb5fadda4be20
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-01-21 18:27:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,17 +1,7 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-class Solution
-{
-    public:
-    //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
-    {
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& price) {
+        int n=price.size();
         vector<int> leftGreater(n,-1);
        
         stack<pair<int,int>> s;
@@ -45,36 +35,6 @@ class Solution
             leftGreater[i] = i-leftGreater[i];
         }
         return leftGreater;
+        
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int i,a[n];
-		for(i=0;i<n;i++)
-		{
-			cin>>a[i];
-		}
-		Solution obj;
-		vector <int> s = obj.calculateSpan(a, n);
-		
-		for(i=0;i<n;i++)
-		{
-			cout<<s[i]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
-}
-
-// } Driver Code Ends

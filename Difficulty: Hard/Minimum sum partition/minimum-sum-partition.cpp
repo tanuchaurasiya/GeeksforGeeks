2657cb7c69bf92cbd96defdfa73e5a5482ad8ce8
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-03-29 17:59:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -1,24 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution{
+class Solution {
 
   public:
-	int minDifference(int arr[], int n)  { 
-        int totalSum=0;
-        for(int i=0;i<n;i++) totalSum+=arr[i];
-        vector<vector<int>> dp;
-        dp.resize(n, vector<int>(totalSum+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        
-        if(arr[0]<=totalSum) dp[0][arr[0]]=true;
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        int tar=0;
+        for(auto a:arr) tar+=a;
+
+        vector<vector<int>> dp(n, vector<int>(tar+1,0));
+
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        // cout<<tar<<endl;
+
+        if(arr[0]<=tar) dp[0][arr[0]]=1;
         
         for(int i=1;i<n;i++){
-            for(int t=1;t<=totalSum;t++){
+            for(int t=1;t<=tar;t++){
                 bool nottake = dp[i-1][t];
                 bool take=false;
                 if(arr[i]<=t){
@@ -29,40 +25,13 @@ class Solution{
             }
         }
 
-        int res = INT_MAX;
-        for(int sub1=0;sub1<=totalSum;sub1++){
-            if(dp[n-1][sub1]==true){
-                int sub2=totalSum-sub1;
-                res=min(res, abs(sub1-sub2));
-            }
+        int res=INT_MAX;
+        for(int i=0;i<tar;i++){
+            if(dp[n-1][i]==false) continue;
+            int sum1=i;
+            int sum2=tar-sum1;
+            res=min(res, abs(sum2-sum1));
         }
         return res;
-	} 
-};
-
-
-//{ Driver Code Starts.
-int main() 
-{
-   
-   
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-
-        int a[n];
-        for(int i = 0; i < n; i++)
-        	cin >> a[i];
-
-       
-
-	    Solution ob;
-	    cout << ob.minDifference(a, n) << "\n";
-	     
     }
-    return 0;
-}
-// } Driver Code Ends
+};

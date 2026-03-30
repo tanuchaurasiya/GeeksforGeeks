bedf893853e01045e3146482cfe0130a8575b405
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-03-30 14:12:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
@@ -1,93 +1,49 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int findUltimateParent(vector<int>& parent, int node)
-    {
-        if(parent[node] == node) return node;
-        return parent[node] = findUltimateParent(parent, parent[node]);
+
+    long long find(long long int node, vector<long long int> &graph){
+        if(graph[node]<0) 
+            return node;
+        long long int temp = find(graph[node], graph);
+        graph[node] = temp;
+        return temp;
     }
-    void dsu(vector<int>& parent, vector<int>& size, int u, int v)
-    {
-        int ulp_u = findUltimateParent(parent, u);
-        int ulp_v = findUltimateParent(parent, v);
-        if(ulp_u == ulp_v) return;
-        if(size[ulp_u] < size[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+    
+    bool Union(long long int node1, long long int node2, vector<long long int> &graph){
+        long long int n1 = find(node1, graph);
+        long long int n2 = find(node2,  graph);
+        if(n1==n2)
+            return false;
+        
+        if(abs(graph[n1]) >= abs(graph[n2])) {
+            graph[n1] = graph[n1]+graph[n2];
+            graph[n2] = n1;
         }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        else{
+            graph[n2] = graph[n1] + graph[n2];
+            graph[n1] = n2;
         }
+        
+        return true;
     }
-    int minCost(vector<vector<int>>& houses) {
-        // code here
-        int n = houses.size();
-        vector<int> parent(n);
-        for(int i = 0; i < n; i++) parent[i] = i;
-        vector<int> size(n, 1);
-        vector<vector<int>> edges;
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i+1; j < n; j++)
-            {
-                int dist = abs(houses[i][0]-houses[j][0]) + abs(houses[i][1]-houses[j][1]);
-                edges.push_back({i, j, dist});
-            }
+    int minCost(vector<vector<int>>& points) {
+        long long int leng = points.size();
+        
+        vector<vector<long long int>> v;
+        for(long long int i=0;i<leng;i++){
+            for(long long int j=i+1;j<leng;j++)
+                v.push_back({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i,j});
         }
-        sort(edges.begin(), edges.end(), [](vector<int>& a, vector<int>& b)
-        {
-            return a[2] < b[2];
-        });
-        int ans = 0;
-        for(vector<int>& i: edges)
-        {
-            if(findUltimateParent(parent, i[0]) != findUltimateParent(parent, i[1]))
-            {
-                ans += i[2];
-                dsu(parent, size, i[0], i[1]);
-            }
+        
+        sort(v.begin(), v.end());
+        vector<long long int> graph(leng,-1);
+        int res=0;
+        
+        for(auto temp:v){
+            if(Union(temp[1], temp[2], graph))
+                res+=temp[0];
         }
-        return ans;
+        
+        return res;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> edges;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 2; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        Solution obj;
-
-        cout << obj.minCost(edges);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends

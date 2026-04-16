0cf07f9d3be1915e6b59bf1151bffa6e1f0860de
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-04-16 21:09:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int myAtoi(string &s) {
+        int sign = 1, r = 0, i = 0;
+        while(s[i] == ' ')      i++;        // ignore whitespaces
+        if(s[i] == '-' || s[i] == '+'){
+            if(s[i++] == '-')       sign = -1;
+        }
+        while(s[i] >= '0' && s[i] <= '9'){ 
+            if(r > INT_MAX/10 || r == INT_MAX/10 && s[i]-'0' > 7)     
+                return      sign == 1 ? INT_MAX : INT_MIN;
+            r = 10*r+(s[i++]-'0');
+        }
+        return  r*sign;
+    }
+};

daf3fb377384bfa59986a7b458ea36378974e8e2
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-04-19 12:55:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -1,57 +1,20 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the next greater element for each element of the array.
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
-        vector<long long> res(n);
-        
-        stack<long long> st;
-        
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                res[i]=-1;
-                st.push(arr[i]);
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        stack<int> st;
+        int n=arr.size();
+        vector<int> res(n,-1);
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.empty()){st.push(arr[i]); continue;}
+            while(!st.empty() && st.top()<=arr[i])
+            {
+                st.pop();
             }
-            else{
-                while((!st.empty()) && arr[i]>=st.top()){
-                    st.pop();
-                }
-                if(st.empty()) res[i]=-1;
-                else res[i]=st.top();
-                
-                st.push(arr[i]);
-            }
+            if(!st.empty()) res[i]=st.top();
+            st.push(arr[i]);
         }
         return res;
     }
+    
 };
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        
-        int n;
-        cin>>n;
-        vector<long long> arr(n);
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        
-        Solution obj;
-        vector <long long> res = obj.nextLargerElement(arr, n);
-        for (long long i : res) cout << i << " ";
-        cout<<endl;
-    }
-	return 0;
-}
-// } Driver Code Ends

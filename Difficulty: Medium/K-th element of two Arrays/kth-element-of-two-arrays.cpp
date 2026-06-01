f9498069575f0fbc6b03bccb47ca42fe99b96d51
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-06-01 19:22:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,53 +1,55 @@
 class Solution {
   public:
     int kthElement(vector<int> &arr1, vector<int> &arr2, int k) {
-        int i=0;
-        int j=0;
-        int n=arr1.size();
-        int m = arr2.size();
-        k--;
-        while(k && i<n && j<m){
-            if(arr1[i]>arr2[j]){
-                j++;
-                k--;
-            }
-            else if(arr1[i]<arr2[j]){
-                i++;
-                k--;
-            } 
-            else{
-                if(k>=2){
-                    i++;
-                    j++;
-                    k-=2;
-                }
-                else{
-                    i++;
-                    k-=1;
-                }
-            }
+int n1 = arr1.size();
+        int n2 = arr2.size();
+        if(n1>n2){
+            return kthElement(arr2, arr1, k);
         }
-        if(k==0){
-            if(i<n && j<m){
-                if(arr1[i]>arr2[j]) return arr2[j];
-                return arr1[i];
+        int low=max(0,k-n2);
+        int high=min(n1,k);
+        int midIdx=k;
+        while(low<=high)
+        {
+
+            int mid1 = (low+high)/2;
+            int mid2 = midIdx-mid1;
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int r1=INT_MAX;
+            int r2=INT_MAX;
+            
+            if(mid1>0){
+                l1=arr1[mid1-1];
             }
             
-            if(i<n) return arr1[i];
-            return arr2[j];
-        } 
-        while(k && i<n) {
-           i++;
-           k--;
+            if(mid2>0){
+                l2=arr2[mid2-1];
+            }
+            
+            if(mid1<n1){
+                r1=arr1[mid1];
+            }
+            
+            if(mid2<n2){
+                r2=arr2[mid2];
+            }
+            
+            if(l1>r2){
+                high=mid1-1;
+            }
+            
+            else if(l2>r1){
+                low=mid1+1;
+            }
+            
+            else{
+                
+                return double(max(l1,l2));
+            }
+            
         }
-        if(k==0) return arr1[i];
-        
-        while(k && j<m) {
-           j++;
-           k--;
-        }
-        if(k==0) return arr2[j];
-        return -1;
+        return 0;
         
     }
 };

780a882933daed52ee64195dc7fdf9ec99dcc609
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2026-06-15 19:05:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,20 +1,26 @@
 class Solution {
   public:
     int minSuperSeq(string &s1, string &s2) {
-        int m=s1.length();
-        int n=s2.length();
-        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
-
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        int n1=s1.size();
+        int n2=s2.size();
+        
+        vector<vector<int>>dp(n1+1, vector<int>(n2+1,0));
+        
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(s1[i-1]==s2[j-1]) {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        return m+n-dp[m][n];
+        // cout<<dp[n1][n2]<<endl;
+        
+        return dp[n1][n2] + (n1-dp[n1][n2]) + (n2-dp[n1][n2]);
         
     }
 };
